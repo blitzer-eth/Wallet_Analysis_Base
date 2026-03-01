68d000dd0e34c10f8ceea0fe8ef973f2bb86db74
blitzer <126232604+blitzer-eth@users.noreply.github.com>
2026-02-28 16:49:23 -0800

    Update [Base] Daily and Total NFTs Sold.sql

--- [Base] Daily and Total NFTs Sold.sql
+++ [Base] Daily and Total NFTs Sold.sql
@@ -15,45 +15,45 @@ address AS (
   SELECT FROM_HEX(LOWER(REPLACE('{{wallet address:}}', '0x', ''))) AS addr
 ),
 
--- Combine ERC721 and ERC1155 (Single & Batch) transfers
+-- Combine ERC721 and ERC1155 (Single & Batch) transfers for both inbound and outbound
 all_transfers AS (
-  -- ERC721 Sales
+  -- ERC721
   SELECT 
     evt_block_time,
     evt_block_date,
-    'ERC721' as standard
+    CASE WHEN "from" = addr THEN 'SELL' ELSE 'BUY' END AS direction
   FROM erc721_base.evt_Transfer
   CROSS JOIN address
   CROSS JOIN time_filter
-  WHERE "from" = addr
+  WHERE ("from" = addr OR "to" = addr)
     AND evt_block_time >= time_filter.start_date
     AND evt_block_date >= CAST(time_filter.start_date AS DATE)
 
   UNION ALL
 
-  -- ERC1155 Sales (Single)
+  -- ERC1155 (Single)
   SELECT 
     evt_block_time,
     evt_block_date,
-    'ERC1155' as standard
+    CASE WHEN "from" = addr THEN 'SELL' ELSE 'BUY' END AS direction
   FROM erc1155_base.evt_TransferSingle
   CROSS JOIN address
   CROSS JOIN time_filter
-  WHERE "from" = addr
+  WHERE ("from" = addr OR "to" = addr)
     AND evt_block_time >= time_filter.start_date
     AND evt_block_date >= CAST(time_filter.start_date AS DATE)
     
   UNION ALL
 
-  -- ERC1155 Sales (Batch)
+  -- ERC1155 (Batch)
   SELECT 
     evt_block_time,
     evt_block_date,
-    'ERC1155' as standard
+    CASE WHEN "from" = addr THEN 'SELL' ELSE 'BUY' END AS direction
   FROM erc1155_base.evt_TransferBatch
   CROSS JOIN address
   CROSS JOIN time_filter
-  WHERE "from" = addr
+  WHERE ("from" = addr OR "to" = addr)
     AND evt_block_time >= time_filter.start_date
     AND evt_block_date >= CAST(time_filter.start_date AS DATE)
 ),
@@ -61,7 +61,8 @@ all_transfers AS (
 daily_stats AS (
   SELECT
     DATE_TRUNC('day', evt_block_time) AS day,
-    COUNT(*) AS nfts_sold_daily
+    SUM(CASE WHEN direction = 'SELL' THEN 1 ELSE 0 END) AS nfts_sold_daily,
+    SUM(CASE WHEN direction = 'BUY' THEN -1 ELSE 0 END) AS nfts_bought_daily
   FROM all_transfers
   GROUP BY 1
 )
@@ -69,6 +70,8 @@ daily_stats AS (
 SELECT
   DATE_FORMAT(day, '%Y/%m/%d') AS date,
   nfts_sold_daily,
-  SUM(nfts_sold_daily) OVER (ORDER BY day) AS nfts_sold_total
+  nfts_bought_daily,
+  SUM(nfts_sold_daily) OVER (ORDER BY day) AS nfts_sold_total,
+  SUM(nfts_bought_daily) OVER (ORDER BY day) AS nfts_bought_total
 FROM daily_stats
 ORDER BY day DESC;
